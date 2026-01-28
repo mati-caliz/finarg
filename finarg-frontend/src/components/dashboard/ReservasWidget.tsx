ad5cb759ac6d8e6b06c3afbc2d0a1d1cc009680b
mati.caliz <69267208+mati-caliz@users.noreply.github.com>
2026-01-28 17:48:36 -0300

    Fix QuoteCardProps export in QuoteCard and DolarCard

--- finarg-frontend/src/components/dashboard/ReservasWidget.tsx
+++
@@ -1,4 +0,0 @@
-'use client';
-
-export { ReservesWidget, ReservesWidget as ReservasWidget } from './ReservesWidget';
-export type { ReservesWidgetProps, ReservesWidgetProps as ReservasWidgetProps } from './ReservesWidget';
