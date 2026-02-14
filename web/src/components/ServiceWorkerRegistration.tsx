2d56d55ac2c6913afbda124b16c41d13e6f517c9
mati.caliz <matias.caliz@despegar.com>
2026-02-14 13:09:58 -0300

    fix

--- web/src/components/ServiceWorkerRegistration.tsx
+++ web/src/components/ServiceWorkerRegistration.tsx
@@ -5,29 +5,9 @@ import { useEffect } from "react";
 export function ServiceWorkerRegistration() {
   useEffect(() => {
     if (typeof window !== "undefined" && "serviceWorker" in navigator) {
-      navigator.serviceWorker
-        .register("/sw.js")
-        .then((registration) => {
-          // eslint-disable-next-line no-console
-          console.log("[SW] Service Worker registered:", registration.scope);
-
-          // Check for updates
-          registration.addEventListener("updatefound", () => {
-            const newWorker = registration.installing;
-            if (newWorker) {
-              newWorker.addEventListener("statechange", () => {
-                if (newWorker.state === "installed" && navigator.serviceWorker.controller) {
-                  // New content is available
-                  // eslint-disable-next-line no-console
-                  console.log("[SW] New content available, refresh to update");
-                }
-              });
-            }
-          });
-        })
-        .catch((error) => {
-          console.error("[SW] Service Worker registration failed:", error);
-        });
+      navigator.serviceWorker.register("/sw.js").catch((error) => {
+        console.error("[SW] Service Worker registration failed:", error);
+      });
     }
   }, []);
 
